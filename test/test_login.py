62cc5f0d897bb556d0313c1f9c635f33ead374bd
prajit-shrestha <134131021+prajit-shrestha@users.noreply.github.com>
2026-07-20 15:45:22 +0545

    feat: add Allure reporting integration and gitignore

--- test/test_login.py
+++ test/test_login.py
@@ -2,15 +2,23 @@ import pytest
 from page_object.login_page import LoginPage
 from config.config import BASE_URL, USERNAME, PASSWORD, Inventory_url
 from utilities.logger import get_logger
+import allure
 
 logger = get_logger()
+
+@allure.title("Verify valid user login")
+@allure.description("Verify user can login successfully with valid credentials")
 def test_login(driver):
     driver.get(BASE_URL)
     logger.info("Starting login test")
     login = LoginPage(driver)
-    login.enter_username(USERNAME)
-    login.enter_password(PASSWORD)
-    login.click_login()
+    with allure.step("Enter username"):
+        login.enter_username(USERNAME)
 
-    url = "https://www.saucedemo.com/inventory.html"
-    assert driver.current_url == url
+    with allure.step("Enter password"):
+        login.enter_password(PASSWORD)
+    with allure.step("Click login button"):
+        login.click_login()
+    with allure.step("Verify user is redirected to inventory page"):
+        url = "inventory.html"
+    assert url in driver.current_url
